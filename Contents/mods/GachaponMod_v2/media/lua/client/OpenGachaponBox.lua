b95e3c21115b7403c5bbdb2c8c00cf7a34364705
jkmenco <jkmenco@gmail.com>
2023-10-17 16:47:49 -0500

    funcion de abrir gachapon, modificado

--- Contents/mods/GachaponMod_v2/media/lua/client/OpenGachaponBox.lua
+++ Contents/mods/GachaponMod_v2/media/lua/client/OpenGachaponBox.lua
@@ -2,6 +2,8 @@ function ProcessMyItem(item)
     local player = getSpecificPlayer(0)
     if player then
         player:getInventory():Remove(item)
-        player:getInventory():AddItem(Base.Pistol) -- Agregar el nuevo item al inventario
+        local items = dofile("Contents/mods/GachaponMod_v2/media/scripts/items/Gachapon_obtainable_items.lua") -- Importar el array de objetos predefinidos
+        local randomItem = items[math.random(#items)] -- Seleccionar un objeto aleatorio del array
+        player:getInventory():AddItem(randomItem) -- Agregar el nuevo objeto al inventario
     end
 end
---
+++ Contents/mods/GachaponMod_v2/media/scripts/items/Gachapon_obtainable_items.lua
@@ -0,0 +1,2 @@
+local items = {"Base.Pistol", "Base.Axe", "Base.Shotgun", "Base.KitchenKnife"} -- Array de objetos predefinidos
+return items -- Devolver el array para que pueda ser importado en otros archivos
